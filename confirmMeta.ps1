5997354c95ae563c92b7fc1b39d3b2a855ef8d3e
Eno <m-enokiya@climb-net.co.jp>
2019-03-13 14:59:27 +0900

    new file

---
+++ confirmMeta.ps1
@@ -0,0 +1,12 @@
+﻿$files = Get-ChildItem 
+#ファイル処理する関数だよ
+function doCopy( $1 ) {
+    #dupがついてない元ファイルを作成するよ
+    $sourceFile = $1.FullName.Substring( 0, $1.FullName.Length - 4 )
+    #dupが無いファイルがあるか確認するよ
+    $1.DirectoryName + "っす"
+    $sourceFile
+    $1.FullName + " だよ"
+}
+#ファイル分ループさせる
+$files | ForEach-Object -Begin {"### Go! ###"} -Process {doCopy $_} -End {"### Finish! ###"}
